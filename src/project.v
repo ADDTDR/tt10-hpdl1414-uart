d9ce80f6d6bbbff8d156a5afe300ab2e379bbf8a
atudori <atudoroi@yahoo.com>
2025-02-20 17:16:53 +0100

    Set uio pin directions

--- src/project.v
+++ src/project.v
@@ -41,6 +41,8 @@ pmod_1414 pmod_1414 (
 
 );
 
+
+ assign uio_oe = 8'b1111_1110;
   // // All output pins must be assigned. If not used, assign to 0.
   // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   // assign uio_out = 0;
